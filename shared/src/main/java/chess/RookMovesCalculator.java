ad7f3ec68bbcbf68b241382d4ef675b780076ab0
alpasaam <alpasaam422@gmail.com>
2025-01-22 09:44:04 -0700

    did everything but the pawn

--- shared/src/main/java/chess/RookMovesCalculator.java
+++ shared/src/main/java/chess/RookMovesCalculator.java
@@ -1,9 +1,54 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class RookMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new ArrayList<>();
+        ChessGame.TeamColor myColor = board.getPiece(myPosition).getTeamColor();
+        int[] directions = {-1,1};
+        for (int dx : directions) {
+            int x = myPosition.getRow();
+            int y = myPosition.getColumn();
+            while (true) {
+                x += dx;
+                ChessPosition newPosition = new ChessPosition(x, y);
+                if (!newPosition.isValid()) {
+                    break;
+                }
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                } else {
+                    if (piece.getTeamColor() != myColor) {
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    }
+                    break;
+                }
+            }
+        }
+
+        for (int dy : directions) {
+            int x = myPosition.getRow();
+            int y = myPosition.getColumn();
+            while (true) {
+                y += dy;
+                ChessPosition newPosition = new ChessPosition(x, y);
+                if (!newPosition.isValid()) {
+                    break;
+                }
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece == null) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                } else {
+                    if (piece.getTeamColor() != myColor) {
+                        moves.add(new ChessMove(myPosition, newPosition, null));
+                    }
+                    break;
+                }
+            }
+        }
+        return moves;
     }
 }
