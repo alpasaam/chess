7f164c79ddcbe166b7fa8e0b7f0455237718899c
alpasaam <alpasaam422@gmail.com>
2025-01-22 09:30:48 -0700

    implemented KingMoves and KnightMoves

--- shared/src/main/java/chess/KingMovesCalculator.java
+++ shared/src/main/java/chess/KingMovesCalculator.java
@@ -1,9 +1,32 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class KingMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new ArrayList<>();
+        int[] directions = {-1, 0, 1};
+        ChessGame.TeamColor myColor = board.getPiece(myPosition).getTeamColor();
+        for (int dx : directions) {
+            for (int dy : directions) {
+                if (dx == 0 && dy == 0) {
+                    continue;
+                }
+                int x = myPosition.getRow();
+                int y = myPosition.getColumn();
+                x += dx;
+                y += dy;
+                ChessPosition newPosition = new ChessPosition(x, y);
+                if (!newPosition.isValid()) {
+                    continue;
+                }
+                ChessPiece piece = board.getPiece(newPosition);
+                if (piece == null || piece.getTeamColor() != myColor) {
+                    moves.add(new ChessMove(myPosition, newPosition, null));
+                }
+            }
+        }
+        return moves;
     }
 }
--- shared/src/main/java/chess/KnightMovesCalculator.java
+++ shared/src/main/java/chess/KnightMovesCalculator.java
@@ -1,9 +1,26 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 
 public class KnightMovesCalculator implements PieceMovesCalculator {
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
-        return null;
+        Collection<ChessMove> moves = new ArrayList<>();
+        int[] dx = {1, 2, 2, 1, -1, -2, -2, -1};
+        int[] dy = {2, 1, -1, -2, -2, -1, 1, 2};
+        ChessGame.TeamColor myColor = board.getPiece(myPosition).getTeamColor();
+        for (int i = 0; i < 8; i++) {
+            int x = myPosition.getRow() + dx[i];
+            int y = myPosition.getColumn() + dy[i];
+            ChessPosition newPosition = new ChessPosition(x, y);
+            if (!newPosition.isValid()) {
+                continue;
+            }
+            ChessPiece piece = board.getPiece(newPosition);
+            if (piece == null || piece.getTeamColor() != myColor) {
+                moves.add(new ChessMove(myPosition, newPosition, null));
+            }
+        }
+        return moves;
     }
 }
