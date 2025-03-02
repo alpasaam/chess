7fbbf7deb26ad0186a0e84908ebb826d66a8376f
alpasaam <alpasaam422@gmail.com>
2025-03-02 11:13:48 -0700

    create base Handler.java class

---
+++ server/src/main/java/server/Handler.java
@@ -0,0 +1,9 @@
+package server;
+
+public class Handler {
+    //this is where you can have a toJson method that takes in a list of objects and returns a json string and
+    // a fromJson method that takes in a json string and returns a list of objects
+
+    // you can also authenticate users here
+
+}
