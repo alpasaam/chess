4c8cd818f710e4a804e6cb549b03064fb92692ec
alpasaam <alpasaam422@gmail.com>
2025-03-02 11:35:33 -0700

    added methods for DAO and GameData record

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,8 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    public void createAuth(AuthData authData);
+    public void getAuth(String authToken) throws DataAccessException;
 }
--- server/src/main/java/dataaccess/UserDAO.java
+++ server/src/main/java/dataaccess/UserDAO.java
@@ -1,4 +1,6 @@
 package dataaccess;
 
 public interface UserDAO {
+    public void createUser(String username, String password, String email);
+    public void getUser(String username);
 }
