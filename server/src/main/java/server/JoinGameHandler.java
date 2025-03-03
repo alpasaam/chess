dd2b8b668a7530f2445e63d961871f52b6de4a61
alpasaam <alpasaam422@gmail.com>
2025-03-02 23:30:00 -0700

    fixed error for constructor

--- server/src/main/java/server/JoinGameHandler.java
+++ server/src/main/java/server/JoinGameHandler.java
@@ -1,9 +1,15 @@
 package server;
 
+import service.GameService;
 import spark.Request;
 import spark.Response;
 
 public class JoinGameHandler {
+    final GameService gameService;
+    public JoinGameHandler(GameService gameService) {
+        this.gameService = gameService;
+    }
+
     public Object joinGame(Request request, Response response) {
         return null;
     }
