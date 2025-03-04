00aedb946c9e4e2cd9f81fb3ac02e4a0cf85a44f
alpasaam <alpasaam422@gmail.com>
2025-03-03 21:10:29 -0700

    changed StatusCode to statusCode

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -56,7 +56,7 @@ public class Server {
     }
 
     private void exceptionHandler(ResponseException ex, Request req, Response res) {
-        res.status(ex.StatusCode());
+        res.status(ex.statusCode());
         res.body(ex.toJson());
     }
 }
